5eaa82b751905a5f25d5da2f2602ab33ee825903
Jaydeboy <jn587256@gmail.com>
2020-06-02 00:42:39 -0400

    handler update

--- src/main/java/com/router/handlers/Social_Security_Handler.java
+++ src/main/java/com/router/handlers/Social_Security_Handler.java
@@ -7,39 +7,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.stream.Collectors;
 
 
 public class Social_Security_Handler {
 
+    private static URL url = null;
+    private static  HttpURLConnection connection = null;
+
     public static void getHearingOfficeList(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        URL url = null;
-        BufferedReader br = null;
-        HttpURLConnection connection = null;
+        url = new URL("https://www.ssa.gov/appeals/DataSets/01_NetStat_Report.xml");
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoInput(true);
+        connection.setReadTimeout(15*1000);
+        connection.connect();
 
-        try {
-            url = new URL("https://www.ssa.gov/appeals/DataSets/01_NetStat_Report.xml");
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setDoInput(true);
-            connection.setReadTimeout(15*1000);
-            connection.connect();
+        String result ="";
 
-            String result ="";
-
-            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));){
             result = br.lines().collect(Collectors.joining());
             resp.setStatus(200);
             resp.setHeader("Content-Type", "application/xml");
             resp.getOutputStream().println(result);
+        }
 
-        }catch (IOException e) {
-            e.printStackTrace();
-            resp.setStatus(404);
+            connection.disconnect();
+    }
+    public static void getFieldOfficeList(HttpServletRequest req, HttpServletResponse resp){
 
-        }finally{
-            br.close();
+
+
+
+    }
+    public void toDatabase(HttpServletRequest req, HttpServletResponse resp) throws SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/social_security_data";
+        String pin = "student";
+        String password = "student";
+
+        try(Connection connection = DriverManager.getConnection(url,pin,password)){
+
+
 
 
         }
@@ -47,6 +60,4 @@ public class Social_Security_Handler {
 
 
     }
-
-
 }
